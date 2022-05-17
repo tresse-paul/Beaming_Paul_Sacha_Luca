283f6b528bd3bd5688bb8bd07a9003a9c67c1086
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-17 17:20:31 +0200

    icone plus.vue

---
+++ src/components/icons/plus.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    width="19"
+    height="19"
+    viewBox="0 0 19 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M9.50002 1.16669V17.8334M17.8334 9.50002L1.16669 9.50002"
+      stroke="#D1D5DB"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
