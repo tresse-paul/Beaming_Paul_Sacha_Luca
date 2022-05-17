de34b3a2826653b4743a3a4ee3dd403476ffccdf
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-17 17:20:19 +0200

    icone minus.vue

---
+++ src/components/icons/minus.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    width="15"
+    height="3"
+    viewBox="0 0 15 3"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M13.75 1.5H1.25"
+      stroke="#D1D5DB"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
