56c21780823c7f869ef8772451a84e7cd11b55c2
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-17 17:21:18 +0200

    component Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="
+      flex
+      h-28
+      w-full
+      items-center
+      justify-start
+      border-b border-gray-100
+      bg-gray-50
+      px-2.5
+      pt-5
+      pb-2.5
+    "
+  >
+    <a href="#"><logo /></a>
+  </div>
+</template>
+<script>
+import logo from "./icons/logo.vue";
+
+export default {
+  components: { logo },
+};
+</script>
