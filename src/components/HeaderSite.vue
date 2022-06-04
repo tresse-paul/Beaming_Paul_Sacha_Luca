290dd505ce36e26afd727389e645a14ed37aca8f
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-06-04 17:14:08 +0200

    composant HeaderSite (header de la landing page)

---
+++ src/components/HeaderSite.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-auto w-full items-center justify-between border-b border-gray-100 bg-gray-50 px-4 pb-5 pt-5">
+    <RouterLink to="/"><logo /></RouterLink>
+    <div class="flex flex-col gap-1.5">
+      <span class="block h-1 w-7 rounded-full bg-blue-350"></span>
+      <span class="block h-1 w-7 rounded-full bg-blue-350"></span>
+      <span class="block h-1 w-7 rounded-full bg-blue-350"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardAbo from "../components/CardAbo.vue";
+import logo from "../components/icons/logo.vue";
+
+export default {
+  components: { CardAbo, logo },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,11 @@
 <template>
-  <div class="flex items-center justify-between px-4 pt-5">
-    <logo />
-    <div class="flex flex-col gap-1.5">
-      <span class="block h-1 w-7 rounded-full bg-blue-350"></span>
-      <span class="block h-1 w-7 rounded-full bg-blue-350"></span>
-      <span class="block h-1 w-7 rounded-full bg-blue-350"></span>
-    </div>
-  </div>
+  <HeaderSite />
 </template>
 
 <script>
-import CardAbo from "../components/CardAbo.vue";
-import logo from "../components/icons/logo.vue";
+import HeaderSite from "../components/HeaderSite.vue";
 
 export default {
-  components: { CardAbo, logo },
+  components: { HeaderSite },
 };
 </script>
