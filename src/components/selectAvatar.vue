a5e0649caa79defa3787395b0d742cda50d3460b
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-06-09 15:47:45 +0200

    preparation vue pour avatar

---
+++ src/components/selectAvatar.vue
@@ -0,0 +1,32 @@
+<template></template>
+
+<script >
+// Bibliothèque Firestore : import des fonctions
+import {
+  getFirestore, // Obtenir le Firestore
+  collection, // Utiliser une collection de documents
+  doc, // Obtenir un document par son id
+  getDocs, // Obtenir la liste des documents d'une collection
+  addDoc, // Ajouter un document à une collection
+  updateDoc, // Mettre à jour un document dans une collection
+  deleteDoc, // Supprimer un document d'une collection
+  onSnapshot, // Demander une liste de documents d'une collection, en les synchronisant
+  query, // Permet d'effectuer des requêtes sur Firestore
+  orderBy, // Permet de demander le tri d'une requête query
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+
+// Cloud Storage : import des fonctions
+import {
+  getStorage, // Obtenir le Cloud Storage
+  ref, // Pour créer une référence à un fichier à uploader
+  getDownloadURL, // Permet de récupérer l'adress complète d'un fichier du Storage
+  uploadString, // Permet d'uploader sur le Cloud Storage une image en Base64
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
+
+export default {
+  components: { Avataaars },
+  data() {
+    return {};
+  },
+};
+</script>
