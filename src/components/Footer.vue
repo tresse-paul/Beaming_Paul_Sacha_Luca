e440e6204692a6bc664d084f2e97f1ddd07abd94
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-06-10 09:19:18 +0200

    modification footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex flex-col gap-10 bg-gray-100 py-5">
+  <div class="relative flex flex-col gap-10 bg-gray-100 py-5">
     <div class="gap flex flex-col items-center gap-5">
       <img class="h-auto w-1/2" src="../assets/Logo_ucl.svg" alt="Logo de UCL" />
       <p class="w-3/4 text-center text-gray-400">Nos sources proviennent de diverses recherches menées par l’University College London</p>
     </div>
-    <div class="relative mb-10 flex flex-col gap-5">
+    <div class="mb-10 flex flex-col gap-5">
       <h5 class="font-bold uppercase">nous suivres</h5>
       <div class="flex gap-5">
         <a href="https://www.facebook.com/">
@@ -17,11 +17,11 @@
           <twitter class="scale-125" />
         </a>
       </div>
-      <img class="absolute left-24 -bottom-32 scale-50" src="../assets/Pouce_3D.webp" alt="Pouce en 3D" />
     </div>
     <RouterLink to="/legale">
       <p class="flex justify-center text-xs underline">Mentions légales</p>
     </RouterLink>
+    <img class="absolute left-28 -bottom-16 z-10 scale-50" src="../assets/Pouce_3D.webp" alt="Pouce en 3D" />
   </div>
 </template>
 
